c08c2271f301a50fc5d31bfa89c0c76f7e5b9d82
MaximilianHauschild <84997690+maxxHauschild@users.noreply.github.com>
2022-06-22 11:19:39 +0200

    Working with 3 multiplexers; cleaner code

--- MulitplexLDR.ino
+++ MulitplexLDR.ino
@@ -1,37 +1,40 @@
  /* 
-This program reads values of a set of double 8 channel Multiplexers reading 8 LDR sensors each and stores the values in arrays.
+This program reads values of a set of three 8 channel Multiplexers reading 8 LDR sensors each and stores the values in arrays.
 Completes threshold comparison for further communication
 Based on example by MehranMaleki 
 */
 
 
 //Mulitplexer control pins
-int s0 = 2;
-int s1 = 3;
-int s2 = 4;
+int dPin[6] = {2,3,4,5,6,7};
+
 
 //Muliplex Analog in in "Z" pin
-int zPlanet = A0;
-int zBody = A1;
+char zPin[3] = {A0,A1,A2};
 
 
 //Output value arrays
-float planetOut [8] = {0,0,0,0,0,0,0,0};
-float bodyOut [8] = {0,0,0,0,0,0,0,0};
-//binary arrays
-int binOutPlanet [8] = {0,0,0,0,0,0,0,0}; // 1: planet gone 0: planet there
-int binOutBody [8] = {0,0,0,0,0,0,0,0}; // 1: bodypart gone 0: bodypart there
+float Output [3][8] ={
+{0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0},
+};
+
+//binary arrays, 0 means the value at the LDR is under the threshold and the object is in its place
+int BinOutput [3][8] ={
+{0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0},
+{0,0,0,0,0,0,0,0},
+};
+
 //threshold
 float thresh = 1.0; // 1v of voltage is a good estimate of threshold betwen the v of an ldr that is blacked out and one that isn't
 
 void setup(){
-  pinMode(s0, OUTPUT); 
-  pinMode(s1, OUTPUT); 
-  pinMode(s2, OUTPUT); 
-
-  digitalWrite(s0, LOW);
-  digitalWrite(s1, LOW);
-  digitalWrite(s2, LOW);
+  for (int i = 0; i<6; i++ ){
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);    
+  }
 
   Serial.begin(9600);
 }
@@ -39,52 +42,36 @@ void setup(){
 
 void loop()
 {
-  //Loop through and read all 8 values
-  for(int i = 0; i < 8; i ++){
-      Serial.print("Value at planet ");
+  updateArrays();
+}
+
+void updateArrays() {
+  for ( int n = 0; n<3; n++){
+    for(int i = 0; i<8; i++){
+      Output[n][i] = readLDR (i, zPin[n], n);
+      if (Output[n][i]<= thresh){
+        BinOutput[n][i] = 1;
+      }
+      else {
+        BinOutput [n][i] = 0;
+      }      
+      Serial.print("LDR ");
+      Serial.print(n);
+      Serial.print(".");
       Serial.print(i);
-      Serial.print("is : ");
-      //Serial.println(readLDR(i));
-      planetOut [i] = readLDR (i,zPlanet);
-      Serial.println(planetOut[i]);
-      if (planetOut[i] <= thresh){
-        binOutPlanet[i] = 0;
-      }
-      if (planetOut[i] > thresh){
-        binOutPlanet[i] = 1;
-      }
+      Serial.print(" : ");
+      Serial.print(Output[n][i]);
+      Serial.print("  There? ->   ");
+      Serial.println(BinOutput[n][i]);
       delayInLoop(500);
-    }
-  for(int b = 0; b < 8; b ++ ){
-    Serial.println(binOutPlanet[b]);
+
+    }    
   }
-  for(int i = 0; i < 8; i ++){
-      Serial.print("Value at bodypart ");
-      Serial.print(i);
-      Serial.print("is : ");
-      //Serial.println(readLDR(i));
-      bodyOut [i] = readLDR (i,zBody);
-      Serial.println(bodyOut[i]);
-      if (bodyOut[i] <= thresh){
-        binOutBody[i] = 0;
-      }
-      if (bodyOut[i] > thresh){
-        binOutBody[i] = 1;
-      }
-      delayInLoop(500);
-    }
-  for(int b = 0; b < 8; b ++ ){
-    Serial.println(binOutBody[b]);
-  }
- /*int peterDoesNotKnowHowToCode = analogRead(A0); 
- Serial.println(peterDoesNotKnowHowToCode);*/
 }
 
 
 
-
-float readLDR(int channel, int Z_pin){
-  int controlPin[] = {s0, s1, s2};
+float readLDR(int channel, char zPin, int sect){
 
   int muxChannel[8][3]={
     {0,0,0}, //channel 0
@@ -97,13 +84,13 @@ float readLDR(int channel, int Z_pin){
     {1,1,1}, //channel 7
   };
 
-  //loop through the 3 sig
-  for(int i = 0; i < 3; i ++){
-    digitalWrite(controlPin[i], muxChannel[channel][i]);
+  //loop through the 3 bits
+  for(int i = 0+sect; i < 3+sect; i ++){
+    digitalWrite(dPin[i], muxChannel[channel][i]);
   }
 
   //read the value at the Z pin
-  int val = analogRead(Z_pin);
+  int val = analogRead(zPin);
 
   //return the value
   float voltage = (val * 5.0) / 1024.0; //use for voltage
