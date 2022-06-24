ffeab1237a0d0e51f3969ec4d6bf93a320e1400a
Emiel-Nagel <94544433+Emiel-Nagel@users.noreply.github.com>
2022-06-24 13:56:35 +0200

    Extended the temperature code to 16 leds

--- Temperature_Leds.ino
+++ Temperature_Leds.ino
@@ -1,7 +1,7 @@
 #include <FastLED.h>
 #include <Timer.h>
-#define NUM_LEDS 8
-#define DATA_PIN 3
+#define NUM_LEDS 16
+#define DATA_PIN 13
 
 CRGB leds[NUM_LEDS];
 
@@ -11,8 +11,8 @@ int brightness;
 void setup() {
   Serial.begin(9600);
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  temperature = 0;
-  brightness = 2;
+  temperature = 500;
+  brightness = 1;
 }
 
 void loop() {
@@ -28,8 +28,8 @@ void turnOn() {
   Serial.println (ledDisplay);
   for (int i = 0; i < int(ledDisplay); i++) {
     leds[i].r = i * brightness;
-    leds[i].g = (4 - abs(4 - i)) * brightness;
-    leds[i].b = (8 - i) * brightness;
+    leds[i].g = 1.5 * (NUM_LEDS/2 - abs(NUM_LEDS/2 - i)) * brightness;
+    leds[i].b = (NUM_LEDS - i) * brightness;
   }
   FastLED.show();
 }
