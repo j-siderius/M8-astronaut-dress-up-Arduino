f2192c33b429c4f9d09918d321ea029ab481f860
MaximilianHauschild <84997690+maxxHauschild@users.noreply.github.com>
2022-06-24 10:56:05 +0200

    added comments

--- ServoDials.ino
+++ ServoDials.ino
@@ -15,11 +15,11 @@ bool oxygen = false;
 float oxygenFloat = 20;
 float pressure = 4; 
 
-bool startani;
-bool launchConfirm;
-int startGrav = 1; //gravity at the start of the 
+bool startani; //toggle start up animation
+bool launchConfirm; //toggle rocket launching animation
+int startGrav = 1; //gravity at the start of the launch 
 
-Timer waitLoop = Timer(1500, loop);
+Timer waitLoop = Timer(1500, loop); //still doesn't do it for me, sry
 
 
 int pos[3] = {0,0,0} ;    // variable to store the servo positions
@@ -27,28 +27,30 @@ Servo servos[3];            // Servo objects
 
 void setup() {
   Serial.begin(9600);
-  
+
   for (int i= 0; i<nrSrv; i++){
-    servos[i].attach(startPin + i);
+    servos[i].attach(startPin + i); //connect the servos
   }  
-  startani = false;
+
+  //set state of animations for testing purposes
+  startani = true; 
   launchConfirm = false;
 }
 
 void loop() {
-  if (startani == false){
+  if (startani == true){
     startUp();
   }
   if (launchConfirm == true){
     launchDials();
   }
   else {
-    setDials(gInt, oxygenFloat, pressure);
+    setDials(gInt, oxygenFloat, pressure); 
     startGrav = 1;
   }  
 }
 
-void startUp(){
+void startUp(){   //sweeps the dials from zero to the max and back
 
   for (int n = 0; n< 180; n++){
       for (int i = 0; i < nrSrv; i++ ){
@@ -64,7 +66,7 @@ void startUp(){
      }    
      delayInLoop(n/4);
   }  
-  startani = true;
+  startani = false;
 }
 
 void launchDials(){
@@ -81,19 +83,22 @@ if (startgrav > 0){
 }
 
 
-void setDials(float grav, float press, float oxy) {
+void setDials(float grav, float press, float oxy) { //sets the dials to the mapped position; takes the floating values as parameters
 
-  float mapGrav = map(grav,0,25,180,0);
-  float mapPress = map(press,0,10,180,0);
-  float mapOxy = map(oxy,0,30,180,0);
+  float mapGrav = map(grav,0,25,180,0); //maps gravity, with highest g force in range 25g
+  float mapPress = map(press,0,10,180,0); //maps pressure, with highest pressure at 10 bar
+  float mapOxy = map(oxy,0,30,180,0); //maps oxygen percentage 
   
-  Serial.println(mapGrav);
+  //Serial.println(mapGrav);
+
+  //set the servos
   servos[0].write(mapGrav);
   servos[1].write(mapPress);
   servos[2].write(mapOxy);    
 
 }
 
+//used as a simple delay function
 void delayInLoop(long microSeconds)
 {    
   microSeconds *= 1000;
