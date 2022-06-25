c688784d9659e8359a74d1fb805c82f85adb47fc
Emiel-Nagel <94544433+Emiel-Nagel@users.noreply.github.com>
2022-06-25 20:09:36 +0200

    Create TemperatureLeds.h

---
+++ Classes/TemperatureLeds.h
@@ -0,0 +1,30 @@
+#include <FastLED.h>
+#include "Timer.h"
+#define NUM_LEDS 16
+#define DATA_PIN 13
+
+class tempLed {
+  private:
+    CRGB leds[NUM_LEDS];
+    int temperature;
+    int brightness;
+  public:
+    void turnOn() {
+      int ledDisplay = NUM_LEDS * float(temperature + 200) / 700 + 1;
+      for (int i = 0; i < int(ledDisplay); i++) {
+        leds[i].r = i * brightness;
+        leds[i].g = 1.5 * (NUM_LEDS / 2 - abs(NUM_LEDS / 2 - i)) * brightness;
+        leds[i].b = (NUM_LEDS - i) * brightness;
+      }
+      FastLED.show();
+    }
+
+    void turnOff() {
+      for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i].r = 0;
+        leds[i].g = 0;
+        leds[i].b = 0;
+      }
+      FastLED.show();
+    }
+};
