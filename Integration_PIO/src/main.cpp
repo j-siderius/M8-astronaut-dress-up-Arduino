ad848bba445bfb5a414cbbb38520d02a52026f25
TFvrDev <nick.rius@gmail.com>
2022-06-29 19:20:02 +0200

    added main variables, started implementing function into state machine

--- Integration_PIO/src/main.cpp
+++ Integration_PIO/src/main.cpp
@@ -43,13 +43,39 @@ Multiplexer multiplexer8 = Multiplexer(multiplexerSelectPin1, multiplexerSelectP
 Multiplexer multiplexer16 = Multiplexer(multiplexerSelectPin1, multiplexerSelectPin2, multiplexerSelectPin3, multiplexerReadPin2, multiplexerReadPin3, 20);
 PlanetDetector detector = PlanetDetector(100);
 
+// function definitions
 void turnOffEverything();
+
+// main variables
+bool planetSelected = false;
+bool launching = false;
+bool landed = false;
+bool doorOpen = false;
+int launchSequenceIndex = 0;
+
+// Timers
+Timer prelaunchTimer = Timer(5000);
+Timer ascensionTimer = Timer(5000);
+Timer travelTimer = Timer(0);
+Timer doorOpenTimer = Timer(3333);
+Timer deadOrAliveTimer = Timer(7000);
+// DURATIONS
+// prelaunch: 5s
+// in space: 5s
+// traveltime = traveltime
+// opening hatch: 2s
+// death/alive: 7s
 
 void setup()
 {
   Serial.begin(9600);
   comm = SerialController();
   LCDinit();
+
+  turnOffEverything();
+  pinMode(buttonLEDPin, OUTPUT);
+
+  Serial.println("Initialized");
 }
 
 void loop()
@@ -63,6 +89,36 @@ void loop()
     turnOffEverything();
 
     LCDupdate("...");
+
+    digitalWrite(buttonLEDPin, HIGH);
+
+    if (!planetSelected)
+    { // if no planet is currently selected
+      if (multiplexer8.hasChanged())
+      {                              // check if the multiplexer has changed
+        char planetFuncDef[] = "PA"; // send the current planet values if they have changed
+        comm.sendChars(planetFuncDef, sizeof(planetFuncDef));
+        comm.sendBoolArray(multiplexer8.readValues(), sizeof(8));
+      }
+      if (detector.getPlanetDetected())
+        planetSelected = true; // if a planet is selected, save this
+    }
+    else
+    { // if a planet is selected
+      if (digitalRead(buttonSwitchPin))
+      {
+        comm.sendChar('L'); // send launching command to Python
+        comm.sendBool(true);
+      }
+
+      if (multiplexer16.hasChanged())
+      {                              // check if the multiplexer has changed
+        char planetFuncDef[] = "AA"; // send the current astronaut values if they have changed
+        comm.sendChars(planetFuncDef, sizeof(planetFuncDef));
+        comm.sendBoolArray(multiplexer16.readValues(), sizeof(16));
+      }
+    }
+
     /*
     - general lighting guiding user
     - button lighting up
@@ -76,6 +132,49 @@ void loop()
 
   // launching
   case 1:
+
+    if (launching)
+    {
+      switch (launchSequenceIndex)
+      {
+      case 0:
+        // prelaunch
+        if (prelaunchTimer.check(millis()))
+        {
+          ascensionTimer.start();
+          launchSequenceIndex++;
+        }
+        else
+        {
+        }
+        break;
+
+      case 1:
+        // ascension
+        if (ascensionTimer.check(millis()))
+        {
+          travelTimer.start();
+          launchSequenceIndex++;
+        }
+        else
+        {
+        }
+        break;
+      }
+    }
+    else
+    {
+      launching = true;
+      prelaunchTimer.start();
+      travelTimer.changeDelay(comm.getTravelTime());
+      launchSequenceIndex = 0;
+      // Timer prelaunchTimer = Timer(5000);
+      // Timer ascensionTimer = Timer(5000);
+      // Timer travelTimer = Timer(0);
+      // Timer doorOpenTimer = Timer(2000);
+      // Timer deadOrAliveTimer = Timer(7000);
+    }
+
     /*
     - start launch (fire led ring + smoke)!!!!!!
     - disabling planet selection/astronaut selection
@@ -94,6 +193,33 @@ void loop()
 
   // landed on planet
   case 3:
+    if (landed)
+    {
+      // display dashboard lights etc
+
+      if (doorOpen)  // if the door is open, display granular outputs
+      {
+        // granular outputs
+      }
+
+      // check if the door has already opened
+      if (!doorOpen && doorOpenTimer.check(millis()))
+      {
+        doorOpen = true;
+      }
+    }
+    else
+    {
+      // we have landed, start door open timer
+      landed = true;
+      doorOpenTimer.start();
+
+      // set dashboard outputs
+      oxygenServo.move(comm.getPlanetOxygen());
+      gforceServo.move(comm.getPlanetGForce());
+      pressureServo.move(comm.getPlanetPressure());
+    }
+
     /*
     - displaying accurate planet data
     - displaying granular experience-of-astronaut-data
@@ -142,14 +268,14 @@ void LCDupdate(String name)
 
 void turnOffEverything()
 {
-  LCDupdate("...");           // updates the LCD to display no destination
-  humidifierMosfet.turnOff(); // turn off the humidity
-  smellMosfet.turnOff();      // turn off the smell
-  vapeMosfet.turnOff();       // turn off the vape mosfet
-  vapeFanMosfet.turnOff();    // turn off the vape fan
-  tempFan.tempOff();          // turn off temperature output
-  tempLED.turnOff();          // turn off the temperature LEDs  
-  generalLED.setBrightness(1, 0);  // turn off all general LEDs
+  LCDupdate("");                  // updates the LCD to display no destination
+  humidifierMosfet.turnOff();     // turn off the humidity
+  smellMosfet.turnOff();          // turn off the smell
+  vapeMosfet.turnOff();           // turn off the vape mosfet
+  vapeFanMosfet.turnOff();        // turn off the vape fan
+  tempFan.tempOff();              // turn off temperature output
+  tempLED.turnOff();              // turn off the temperature LEDs
+  generalLED.setBrightness(1, 0); // turn off all general LEDs
   generalLED.setBrightness(2, 0);
   generalLED.setBrightness(3, 0);
   generalLED.setBrightness(4, 0);
@@ -157,4 +283,5 @@ void turnOffEverything()
   oxygenServo.move(0);            // set servos to 0 position
   gforceServo.move(0);
   pressureServo.move(0);
+  digitalWrite(buttonLEDPin, LOW); // turn off the button LED
 }
