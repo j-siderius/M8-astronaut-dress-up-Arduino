17de3169f4a8deed1f30d6cc83f5464bf5ab0be8
TFvrDev <nick.rius@gmail.com>
2022-06-30 19:29:11 +0200

    fixed detector code

--- Integration_PIO/include/PlanetDetector.h
+++ Integration_PIO/include/PlanetDetector.h
@@ -5,39 +5,59 @@
 #include <Wire.h>
 #include <VL53L0X.h>
 
-class PlanetDetector {
-    private:
-        VL53L0X sensor;
-        uint16_t distance;
-        uint16_t range;
+class PlanetDetector
+{
+private:
+    VL53L0X sensor;
+    uint16_t range;
 
-    public:
-        /*!
-        @brief  Planetdetector will check for a planet in the pedestal
-        @param  range   Range at which a planet is considered present in the pedestal (in mm)
-        @return PlanetDetector object
-        */
-        PlanetDetector(int range) {
-            this->range = range;
-            Wire.begin();
-            sensor.setTimeout(500);
-            sensor.init();
-            sensor.setMeasurementTimingBudget(20000);  // enable high speed mode
-        }
+public:
+    uint16_t distance = 10000;
 
-        /*!
-        @brief  reads the sensor value into the class variable
-        */
-        void readSensor() {
-            distance = sensor.readRangeSingleMillimeters();
-        }
+    /*!
+    @brief  Planetdetector will check for a planet in the pedestal
+    @param  range   Range at which a planet is considered present in the pedestal (in mm)
+    @return PlanetDetector object
+    */
+    PlanetDetector(int range)
+    {
+        this->range = range;
+    }
 
-        /*!
-        @brief  checks if a planet is detected in the pedestal or not
-        @return boolean true is planet is detected, false if not
-        */
-        bool getPlanetDetected() {
-            if (distance < range) return true;
-            else return false;
+    /**
+     * @brief starts the detector
+     *
+     */
+    void begin()
+    {
+        Wire.begin();
+        sensor.setTimeout(500);
+        if (!sensor.init())
+        {
+            Serial.println("Failed to detect and initialize sensor!");
+            while (1)
+            {
+            }
         }
+        sensor.setMeasurementTimingBudget(20000); // enable high speed mode
+    }
+    /*!
+    @brief  reads the sensor value into the class variable
+    */
+    void readSensor()
+    {
+        distance = sensor.readRangeSingleMillimeters();
+    }
+
+    /*!
+    @brief  checks if a planet is detected in the pedestal or not
+    @return boolean true is planet is detected, false if not
+    */
+    bool getPlanetDetected()
+    {
+        if (distance < range)
+            return true;
+        else
+            return false;
+    }
 };
