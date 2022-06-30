0e16a98f37efe8479e58fc3d3d22db80cd49e0d7
TFvrDev <nick.rius@gmail.com>
2022-06-30 15:07:05 +0200

    added mapping functions

--- Integration_PIO/include/ServoThing.h
+++ Integration_PIO/include/ServoThing.h
@@ -1,20 +1,75 @@
 #include <Servo.h>
 
-class ServoThing {
-    private:
-        int pin;
-        Servo servo;
+class ServoThing
+{
+private:
+    int pin;
+    float minValue, maxValue;
+    Servo servo;
+    bool logScale;
 
-    public:
-        ServoThing(int pin) {
-            this->pin = pin;
-        }
+public:
+    /*!
+@brief  Constructor for the servo
+@param  pin         Pin where the servo is connected
+@param  minValue    Minimal expected value to be passed to this servo / dial
+@param  maxValue    Maximal expected value to be passed to this servo / dial
+@return ServoController object
+*/
+    ServoThing(int pin, float minValue, float maxValue)
+    {
+        this->pin = pin;
+        this->minValue = minValue;
+        this->maxValue = maxValue;
+        this->logScale = false;
+    }
 
-        void begin() {
-            servo.attach(pin);
-        }
+    /*!
+    @brief  Constructor for the servo
+    @param  pin         Pin where the servo is connected
+    @param  minValue    Minimal expected value to be passed to this servo / dial
+    @param  maxValue    Maximal expected value to be passed to this servo / dial
+    @param  log         Boolean for enabling log mapping
+    @warning    If logarithmic scale is enabled, the minValue will be put to 0!
+    @return ServoController object
+    */
+    ServoThing(int pin, float minValue, float maxValue, bool log)
+    {
+        this->pin = pin;
+        this->logScale = log;
+        if (logScale)
+            this->minValue = 0;
+        else
+            this->minValue = minValue;
+        this->maxValue = maxValue;
+    }
 
-        void move(int position) {
+    /**
+     * @brief Starting the servo
+     * 
+     */
+    void begin()
+    {
+        servo.attach(pin);
+    }
+
+    /*!
+    @brief  Move the servo to specified position
+    @param  value   Input value, will be mapped according to the min- and max value
+    */
+    void move(float value)
+    {
+        if (!logScale)
+        {
+            // normal linear mapping
+            int position = map(value, minValue, maxValue, 0, 180);
             servo.write(position);
         }
+        else
+        {
+            // logarithmic mapping
+            int position = log(value + 1) / log(maxValue) * 180;
+            servo.write(position);
+        }
+    }
 };
